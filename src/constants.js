83623c2634e3fa403c7c0011e5f8fe349abcba0c
calvansaw <calvansaw@hotmail.com>
2023-10-12 01:24:16 +0800

    fix: use CloudFront to serve static website hosted on S3, to resolve cognito require https for auth

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const DOMAIN_URL =
   "https://authcs.auth.ap-southeast-1.amazoncognito.com";
 export const CLIENT_ID = "5tkqkshj5v16aeh665p03ji77a";
 export const REDIRECT_URI =
-  "http://filesapp-bucket.s3-website-ap-southeast-1.amazonaws.com";
+  "https://filesapp-bucket.s3-website-ap-southeast-1.amazonaws.com";
 
 export const AUTHORIZE_URL = `${DOMAIN_URL}/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&scope=resourceidentifier/read%20resourceidentifier/write&redirect_uri=${REDIRECT_URI}`;
 export const TOKEN_URL = `${DOMAIN_URL}/oauth2/token`;
